0072bb8f8b6ccec1e47b7d463cd0d00794f43598
Yangdaehan <yangdh08@naver.com>
2023-11-25 00:15:15 +0900

    clear git cache

--- src/main/java/com/example/taba42/repository/MemberRepository.java
+++ src/main/java/com/example/taba42/repository/MemberRepository.java
@@ -1,2 +1,9 @@
-package com.example.taba42.repository;public interface MemberRepository {
+package com.example.taba42.repository;
+
+import com.example.taba42.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
--- src/main/java/com/example/taba42/service/MemberService.java
+++ src/main/java/com/example/taba42/service/MemberService.java
@@ -1,2 +1,24 @@
-package com.example.taba42.service;public class MemberService {
+package com.example.taba42.service;
+
+import com.example.taba42.domain.Member;
+import com.example.taba42.dto.SignUpRequest;
+import com.example.taba42.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public void addMember(SignUpRequest request) {
+        final Member member = Member.from(request);
+        memberRepository.save(member);
+    }
 }
