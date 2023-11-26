872de177b71459a69a6853f59068054dae6b3bba
Yangdaehan <yangdh08@naver.com>
2023-11-27 00:40:17 +0900

    feat : 회원정보 내보내기

---
+++ src/main/java/com/example/taba42/dto/response/MemberResponse.java
@@ -0,0 +1,39 @@
+package com.example.taba42.dto.response;
+
+import com.example.taba42.domain.Member;
+import lombok.Getter;
+
+@Getter
+public class MemberResponse {
+
+    private Long id;
+
+    private String name;
+
+    private int age;
+
+    private String mid;
+
+    private String phoneNumber;
+
+    public MemberResponse() {
+    }
+
+    public MemberResponse(Long id, String name, int age, String mid, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.mid = mid;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public static MemberResponse from(Member member) {
+        return new MemberResponse(
+                member.getId(),
+                member.getName(),
+                member.getAge(),
+                member.getMid(),
+                member.getPhoneNumber()
+        );
+    }
+}
