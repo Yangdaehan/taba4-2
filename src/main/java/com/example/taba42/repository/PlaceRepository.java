96564438fc054ac82bdff3893e1f768136999a01
Yangdaehan <yangdh08@naver.com>
2023-12-01 09:57:36 +0900

    feat : place DB 테이블 생성

---
+++ src/main/java/com/example/taba42/repository/PlaceRepository.java
@@ -0,0 +1,9 @@
+package com.example.taba42.repository;
+
+import com.example.taba42.domain.Place;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlaceRepository extends JpaRepository<Place, Long> {
+}
