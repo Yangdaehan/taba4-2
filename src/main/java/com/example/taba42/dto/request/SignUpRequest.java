02f81ad583eead9327888d53930770b6c8af84f4
Yangdaehan <yangdh08@naver.com>
2023-12-05 15:50:02 +0900

    chore : ip 주소 변경

--- src/main/java/com/example/taba42/dto/request/SignUpRequest.java
+++ src/main/java/com/example/taba42/dto/request/SignUpRequest.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.Pattern;
 @Getter
 public class SignUpRequest {
 
-    @NotBlank
+    //@NotBlank
     @Pattern(regexp = "^[가-힣]{2,4}$",
             message = "이름은 한국 이름으로 2~4글자 까지 가능합니다.")
     private String name;
@@ -19,15 +19,15 @@ public class SignUpRequest {
     private int age;
 
     @Email(message = "아이디는 이메일 형식이여야합니다.")
-    @NotBlank
+    //@NotBlank
     private String mid;
 
-    @NotBlank
+   // @NotBlank
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{9,16}",
             message = "비밀번호는 영문, 숫자, 특수기호가 적어도 1개 이상씩 포함된 9자 ~ 16자의 비밀번호여야 합니다.")
     private String password;
 
-    @NotBlank
+    //@NotBlank
     private String phoneNumber;
 
     public SignUpRequest() {
